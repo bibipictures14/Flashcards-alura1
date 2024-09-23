60d04b8ae1a4854464472f4c375bf1a17abf5fa6
bibipictures14 <144055575+bibipictures14@users.noreply.github.com>
2024-09-23 08:37:39 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Bruno Mars',
+    'Qual o nome real do Bruno Mars?',
+    'Peter Gene Hernandez, mais conhecido pelo nome artístico Bruno Mars, apelidado de "Novo Rei do Pop"'
+)
+
+criaCartao(
+    'Bruno Mars',
+    'De onde surgiu o nome Bruno Mars?',
+    'Desde novo, ele adotou o apelido de Bruno,porque seu pai pensava que ele se parecia com o lutador profissional Bruno Sammartino.E o Mars (Marte) surgiu só porque muitas garotas dizem que ele era de fora deste mundo'
+)
+
+criaCartao(
+    'Bruno Mars',
+    'Qual música estorou em sua carreira?',
+    '“Just the Way You Are” e “Grenade”'
+)
